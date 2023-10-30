1e0b7d7653bed8607975f402867cf6e13d2912d5
Rijal Ahmad T <rijal@Rijals-Mac-mini.local>
2023-10-30 16:42:08 +0700

    Added component card information Jenis Kelamin, DPT, Data Suara and apply boundary for map

---
+++ assets/theme_assets/js/app.js
@@ -0,0 +1,20 @@
+$("#select-search-provinsi,.kb__select").select2({
+    placeholder: "Provinsi",
+    dropdownCssClass: "category-member",
+    allowClear: true,
+});
+$("#select-search-kabupaten,.kb__select").select2({
+    placeholder: "Kabupaten",
+    dropdownCssClass: "category-member",
+    allowClear: true,
+});
+$("#select-search-kecamatan,.kb__select").select2({
+    placeholder: "Kecamatan",
+    dropdownCssClass: "category-member",
+    allowClear: true,
+});
+$("#select-search-kelurahan,.kb__select").select2({
+    placeholder: "Kelurahan",
+    dropdownCssClass: "category-member",
+    allowClear: true,
+});
--- assets/theme_assets/js/basemap.js
+++ assets/theme_assets/js/basemap.js
@@ -1,4 +1,4 @@
-// const maxBoundArea = L.latLngBounds(L.latLng(-21.41, 153.41), L.latLng(14.3069694978258, 73.65));
+const maxBoundArea = L.latLngBounds(L.latLng(-18.41, 145.41), L.latLng(13.6, 90.65));
 // const basemap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     attribution: '© OpenStreetMap',
 //     minZoom: 5,
@@ -8,7 +8,10 @@
 // maxZoom: 4,
 // center: [-2, 119],
 
-var map = L.map('leaflet-basic').setView([-2, 119], 5);
+var map = L.map('leaflet-basic', {
+    minZoom: 5,
+    maxBounds: maxBoundArea
+}).setView([-2, 119], 5);
 
 map.setMaxZoom(9);
 
